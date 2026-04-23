8bf5b6b8aa4a83f8bfd8fd3cffe9ecc1dd3732ed
Asfak Ahamed <165243010+asfak-am@users.noreply.github.com>
2026-04-23 13:04:21 +0530

    backend deployment ulr updated

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || 'https://rental-life.vercel.app',
+    process.env.CLIENT_URL || 'https://rental-life-manager.onrender.com',
     'http://localhost:5173',
     'http://localhost:5174',
   ],

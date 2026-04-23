35d8c4f2132cf99cefca2b4176d5521b75a3dcfd
Asfak Ahamed <165243010+asfak-am@users.noreply.github.com>
2026-04-23 12:31:26 +0530

    cors url added

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ connectDB()
 const app = express()
 
 app.use(cors({
-  origin: [process.env.CLIENT_URL || 'http://localhost:5173', 'http://localhost:5174'],
+  origin: 'https://rental-life.vercel.app',
   credentials: true,
 }))
 app.use(express.json({ limit: '12mb' }))
